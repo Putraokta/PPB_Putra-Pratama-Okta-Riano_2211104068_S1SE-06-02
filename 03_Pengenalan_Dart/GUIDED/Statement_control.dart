b71f24d416c5ff39c023ac8df39c6a0f846e0a47
Putra Pratama Okta Riano <162243248+Putraokta@users.noreply.github.com>
2024-10-07 20:03:00 +0700

    Create Statement_control.dart

---
+++ 03_Pengenalan_Dart/GUIDED/Statement_control.dart
@@ -0,0 +1,40 @@
+void main(){
+  //Statement Control
+  // if else
+   var open = 8;
+   var close = 17;
+   var now = 13; 
+
+   if(now >= open && now <= close){
+     print("Open now!");
+   } else if(now == 13){
+     print("istirahat sholat jumat");
+   }
+   else {
+     print("Sorry, we're closed.");
+   } // End if
+
+// condition ? True : False
+
+var toko = now > open ? "Toko buka" : "Toko tutup";
+print(toko);
+
+
+ var nilai ='b';
+
+ switch (nilai) {
+   case 'a':
+     print('Nilai yang sangat bagus');
+     break;
+   case 'b':
+     print('Nilai jelek');
+     break;
+   case 'c':
+     print('Nilai sangat jelek');
+   default:
+     print('Nilai default');
+ }
+
+
+
+} // End main
