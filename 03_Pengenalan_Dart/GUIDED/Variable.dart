df5242255beb90d7f279487a6164569a3e23825c
Putra Pratama Okta Riano <162243248+Putraokta@users.noreply.github.com>
2024-10-07 20:08:04 +0700

    Create Variable.dart

---
+++ 03_Pengenalan_Dart/GUIDED/Variable.dart
@@ -0,0 +1,13 @@
+void main() {
+  var name = "Bawor";
+  var age = "23";
+  print("name: $name, usia: $age");
+  
+  String firstName, lastName; // Tipe data String
+  firstName = "Putra";
+  lastName = "Pratama Okta Riano";
+  final cuaca = "Cerah";
+
+  print("nama: $firstName, $lastName" );
+
+}
