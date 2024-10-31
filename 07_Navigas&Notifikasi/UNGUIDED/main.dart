0d2756eb2fe1a7f3f9829708f8962a82cde4fbc4
Putra Pratama Okta Riano <162243248+Putraokta@users.noreply.github.com>
2024-10-31 11:40:27 +0700

    Create main.dart

---
+++ 07_Navigas&Notifikasi/UNGUIDED/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:project_pencaksilat/pages/home_page.dart';
+import 'package:project_pencaksilat/pages/login_page.dart';
+import 'package:project_pencaksilat/pages/user_profile_page.dart';
+
+void main() {
+  runApp(PencakSilatApp());
+}
+
+class PencakSilatApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Pencak Silat Store',
+      theme: ThemeData(primarySwatch: Colors.green),
+      initialRoute: '/login', // Set LoginPage as the initial route
+      routes: {
+        '/login': (context) => LoginPage(),
+        '/home': (context) => HomePage(),
+        '/profile': (context) => UserProfilePage(),
+      },
+    );
+  }
+}
+ 
