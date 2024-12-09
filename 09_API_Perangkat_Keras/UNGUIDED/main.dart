9571f25b82f17e3a55aa3e258d7f9f0189060939
Putra Pratama Okta Riano <162243248+Putraokta@users.noreply.github.com>
2024-12-09 18:15:06 +0700

    Update main.dart

--- 09_API_Perangkat_Keras/UNGUIDED/main.dart
+++ 09_API_Perangkat_Keras/UNGUIDED/main.dart
@@ -1,1 +1,24 @@
+import 'package:flutter/material.dart';
+import 'screens/home_page.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Image Picker Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.deepPurple,
+        colorScheme: ColorScheme.fromSwatch(primarySwatch: Colors.deepPurple)
+            .copyWith(secondary: Colors.pinkAccent),
+        scaffoldBackgroundColor: Colors.white,
+        canvasColor: Colors.white,
+      ),
+      home: HomePage(),
+    );
+  }
+}
 
