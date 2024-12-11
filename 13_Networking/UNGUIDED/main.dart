e49baa36464d10a1a0fd7ff044950c8d71942f42
Putra Pratama Okta Riano <162243248+Putraokta@users.noreply.github.com>
2024-12-11 19:20:49 +0700

    Update main.dart

--- 13_Networking/UNGUIDED/main.dart
+++ 13_Networking/UNGUIDED/main.dart
@@ -1,1 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:pertemuan13_2/add_note_page.dart';
+import 'home_page.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Simple Notes App',
+      initialRoute: '/',
+      getPages: [
+        GetPage(name: '/', page: () => HomePage()),
+        GetPage(name: '/add', page: () => AddNotePage()),
+      ],
+    );
+  }
+}
 
