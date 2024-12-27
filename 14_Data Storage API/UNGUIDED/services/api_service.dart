271ad62f01e6f8110db852b47b9bd5410b8eef92
Putra Pratama Okta Riano <162243248+Putraokta@users.noreply.github.com>
2024-12-28 05:15:08 +0700

    Update api_service.dart

--- 14_Data Storage API/UNGUIDED/services/api_service.dart
+++ 14_Data Storage API/UNGUIDED/services/api_service.dart
@@ -1,23 +1,53 @@
-class Post {
-  int id;
-  String title;
-  String body;
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import 'package:unguided/models/post_models.dart';
 
-  Post({required this.id, required this.title, required this.body});
+class ApiService {
+  final String baseUrl = "https://jsonplaceholder.typicode.com";
 
-  factory Post.fromJson(Map<String, dynamic> json) {
-    return Post(
-      id: json['id'],
-      title: json['title'],
-      body: json['body'],
+  Future<List<Post>> fetchPosts() async {
+    final response = await http.get(Uri.parse('$baseUrl/posts'));
+    if (response.statusCode == 200) {
+      List jsonResponse = json.decode(response.body);
+      return jsonResponse.map((post) => Post.fromJson(post)).toList();
+    } else {
+      throw Exception('Failed to load posts');
+    }
+  }
+
+  Future<Post> createPost(Post post) async {
+    final response = await http.post(
+      Uri.parse('$baseUrl/posts'),
+      headers: {'Content-Type': 'application/json'},
+      body: json.encode(post.toJson()),
     );
+
+    if (response.statusCode == 201) {
+      return Post.fromJson(json.decode(response.body));
+    } else {
+      throw Exception('Failed to create post');
+    }
   }
 
-  Map<String, dynamic> toJson() {
-    return {
-      'id': id,
-      'title': title,
-      'body': body,
-    };
+  Future<Post> updatePost(int id, Post post) async {
+    final response = await http.put(
+      Uri.parse('$baseUrl/posts/$id'),
+      headers: {'Content-Type': 'application/json'},
+      body: json.encode(post.toJson()),
+    );
+
+    if (response.statusCode == 200) {
+      return Post.fromJson(json.decode(response.body));
+    } else {
+      throw Exception('Failed to update post');
+    }
   }
-}  
+
+  Future<void> deletePost(int id) async {
+    final response = await http.delete(Uri.parse('$baseUrl/posts/$id'));
+
+    if (response.statusCode != 200) {
+      throw Exception('Failed to delete post');
+    }
+  }
+}
